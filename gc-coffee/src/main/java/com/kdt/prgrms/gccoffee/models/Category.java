12ac22bee1cdbbc57bfbe56f67f334be61b2a410
UJ15 <dbwogml15@naver.com>
2022-05-04 02:51:26 +0900

    :sparkles: feat : Product 엔티티 생성

---
+++ gc-coffee/src/main/java/com/kdt/prgrms/gccoffee/models/Category.java
@@ -0,0 +1,6 @@
+package com.kdt.prgrms.gccoffee.models;
+
+public enum Category {
+
+    COFFEE_BEAN_PACKAGE
+}
---
+++ gc-coffee/src/main/java/com/kdt/prgrms/gccoffee/models/Product.java
@@ -0,0 +1,89 @@
+package com.kdt.prgrms.gccoffee.models;
+
+
+import java.time.LocalDateTime;
+
+public class Product {
+
+    private final long productId;
+    private String productName;
+    private Category category;
+    private final long price;
+    private String description;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Product(long productId, String productName, Category category, long price) {
+
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Product(long productId, String productName, Category category, long price, String description, LocalDateTime createdAt, LocalDateTime updatedAt) {
+
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public long getProductId() {
+
+        return productId;
+    }
+
+    public String getProductName() {
+
+        return productName;
+    }
+
+    public Category getCategory() {
+
+        return category;
+    }
+
+    public long getPrice() {
+
+        return price;
+    }
+
+    public String getDescription() {
+
+        return description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+
+        return updatedAt;
+    }
+
+    public void updateProductName(String productName) {
+
+        this.productName = productName;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void updateDescription(String description) {
+
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void updateCategory(Category category) {
+
+        this.category = category;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
